4b29b7bc3e3b0eaedfcee5a7fe68aa9f2a1028e4
Rayan Wilbert <rayan.rmw@gmail.com>
2022-10-23 17:36:45 -0300

    Add text fonts

--- App.tsx
+++ App.tsx
@@ -1,13 +1,38 @@
 import { StyleSheet, View } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
+import { useFonts } from 'expo-font';
+import { Inter_400Regular, Inter_500Medium } from '@expo-google-fonts/inter';
+import {
+  Rajdhani_500Medium,
+  Rajdhani_700Bold,
+} from '@expo-google-fonts/rajdhani';
+import * as SplashScreen from 'expo-splash-screen';
 
 import { SignIn } from './src/screens/SignIn';
+import { useCallback } from 'react';
+
+SplashScreen.preventAutoHideAsync();
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_500Medium,
+    Rajdhani_500Medium,
+    Rajdhani_700Bold,
+  });
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) return null;
+
   return (
-    <View style={styles.container}>
+    <View style={styles.container} onLayout={onLayoutRootView}>
       <SignIn />
-      <StatusBar style="light" translucent />
+      <StatusBar style='light' translucent />
     </View>
   );
 }
