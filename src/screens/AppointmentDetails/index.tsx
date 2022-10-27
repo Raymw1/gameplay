bd2da2e0a178eb569b6b38a99dff57e65955c820
Rayan Wilbert <rayan.rmw@gmail.com>
2022-10-27 14:47:13 -0300

    Add backgroundImage in Details page

--- src/screens/AppointmentDetails/index.tsx
+++ src/screens/AppointmentDetails/index.tsx
@@ -1,9 +1,10 @@
-import { View } from 'react-native';
+import { ImageBackground, Text, View } from 'react-native';
 import { BorderlessButton } from 'react-native-gesture-handler';
 import { Fontisto } from '@expo/vector-icons';
 
 import { styles } from './styles';
 import { theme } from '../../global/styles/theme';
+import bannerImg from '../../assets/banner.png';
 
 import { Background } from '../../components/Background';
 import { Header } from '../../components/Header';
@@ -20,6 +21,15 @@ export function AppointmentDetails() {
   return (
     <Background>
       <Header title='Details' action={<ShareAppointment />} />
+      <ImageBackground source={bannerImg} style={styles.banner}>
+        <View style={styles.bannerContent}>
+        <Text style={styles.title}>Legendaries</Text>
+        <Text style={styles.subtitle}>
+          It's today that we will reach the challenger without losing a md10
+          match
+        </Text>
+        </View>
+      </ImageBackground>
     </Background>
   );
 }
